b92a58f4dee192367ace54639179b5c69116c624
h8m0ss <danarmenta32@gmail.com>
2023-10-24 15:00:36 -0600

    CHORE: Added react and svelte. FEAT: Added default layout to md files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+const defaultMarkdownLayout = () => {
+  return function (_, file) {
+    file.data.astro.frontmatter.layout = file.data.astro.frontmatter.layout || "../../../layouts/MarkdownLayout.astro";
+  };
+};
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), svelte()],
+  markdown: {
+    remarkPlugins: [defaultMarkdownLayout],
+    shikiConfig: {
+      theme: 'monokai'
+    }
+  }
 });
