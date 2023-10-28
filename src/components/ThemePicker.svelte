a4c03b28dc444f7d4f49975e1ffb022aba3a3d89
h8m0ss <danarmenta32@gmail.com>
2023-10-27 22:33:39 -0600

    STYLE: Added dark mode

--- src/components/ThemePicker.svelte
+++ src/components/ThemePicker.svelte
@@ -1,22 +1,38 @@
-<script lang="typescript">
+<script lang="ts">
+	import { onMount } from "svelte";
 	import { backInOut } from "svelte/easing";
 	import { tweened } from "svelte/motion";
 
-	let toggled = false;
+	let toggled = true;
 
 	const position = tweened(0, {
 		duration: 200,
 		easing: backInOut
 	});
 
-	const toggleAnim = () => {
-		$position = toggled ? 1 : 0;
+	$: $position = toggled ? 1 : 0;
+
+	const runToggle = () => {
+		toggled = !toggled;
+
+		const theme = toggled ? "light" : "dark";
+		localStorage.setItem("theme", theme);
+
+		document.documentElement.setAttribute("data-theme", theme);
 	};
 
-	$: (toggled || true) && toggleAnim();
+	const setSwitch = () => {
+		const theme = localStorage.getItem("theme");
+		toggled = theme === "light";
+	};
+
+	onMount(() => {
+		setSwitch();
+		document.addEventListener("astro:after-swap", setSwitch);
+	});
 </script>
 
-<button class="parent" on:click={() => (toggled = !toggled)}>
+<button class="parent" on:click={runToggle}>
 	<div class="ball" style:transform="translateX({100 * $position}%)" />
 </button>
 
@@ -35,11 +51,13 @@
 		box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
 			rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
 
+		background-color: var(--background);
+
 		div.ball {
 			height: 2rem;
 			width: 2rem;
 			border-radius: 2rem;
-			background-color: #707070;
+			background-color: var(--accent);
 
 			box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
 				rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
