9819d06399148b2c7b89fac3ce945fff4ca849f4
h8m0ss <danarmenta32@gmail.com>
2023-11-08 13:22:39 -0600

    FEAT: Added first plugin to experiment

--- markdown-plugins/experiment/index.ts
+++ markdown-plugins/experiment/index.ts
@@ -3,16 +3,22 @@ import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
-import {unified} from 'unified'
+import remarkCodeDataTitles from '../src/remark-code-data-titles.js'
+import {remark} from 'remark';
+import logger from './logger.js'
 
 const text = `
 # hello world
+
+\`\`\`js[my nice code]
+let x = 100;
+\`\`\`
 `;
 
-const file = await unified()
-  .use(remarkParse)
+const file = await remark()
+  .use(remarkCodeDataTitles)
   .use(remarkRehype)
-  .use(rehypeSanitize)
+  .use([logger])
   .use(rehypeStringify)
   .process(text)
 
