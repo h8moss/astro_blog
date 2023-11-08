f239e188c014abf3bcc62ceca3cf6c0669119e7d
h8m0ss <danarmenta32@gmail.com>
2023-11-08 13:53:00 -0600

    OTHER: Added more to the experiment

--- markdown-plugins/experiment/index.ts
+++ markdown-plugins/experiment/index.ts
@@ -5,6 +5,7 @@ import remarkRehype from "remark-rehype";
 import remarkCodeDataTitles from "../src/remark-code-data-titles.js";
 import { remark } from "remark";
 import logger from "./logger.js";
+import rehypeCodeAddTitles from "../src/rehype-code-add-titles.js";
 
 const text = `
 # hello world
@@ -12,12 +13,16 @@ const text = `
 \`\`\`js[my nice code]
 let x = 100;
 \`\`\`
-`;
+
+\`\`\`ts
+let x = 100;
+\`\`\` `;
 
 const file = await remark()
 	.use(remarkCodeDataTitles)
 	.use(remarkRehype)
 	.use([logger])
+	.use(rehypeCodeAddTitles)
 	.use(rehypeStringify)
 	.process(text);
 
