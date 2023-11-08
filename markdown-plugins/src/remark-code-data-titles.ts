e2d0ea258f138fd9f00ccfbe8e5d6a355a2302b7
h8m0ss <danarmenta32@gmail.com>
2023-11-08 13:22:08 -0600

    FEAT: Created first plugin

---
+++ markdown-plugins/src/remark-code-data-titles.ts
@@ -0,0 +1,60 @@
+import { visit } from "unist-util-visit";
+import { Plugin, Transformer } from "unified";
+import type { Root } from "remark-directive/lib";
+import type {Code} from 'mdast'
+
+const getLangAndTitle = (node: Code): [string|undefined, string|undefined] => {
+  const meta = node.meta;
+  let lang = node.lang;
+
+  if (!lang) {
+    return [undefined, undefined];
+  }
+
+  let title = lang;
+  let fullLang = lang;
+
+  if (meta) {
+    fullLang += ' ' + meta;
+    node.meta = null;
+  }
+
+  if (fullLang.includes('[') && fullLang.includes(']')) {
+    const openingIndex = fullLang.indexOf('[')
+    const closingIndex = fullLang.indexOf(']')
+    title = fullLang.substring(openingIndex+1, closingIndex);
+    lang = fullLang.substring(0, openingIndex);
+  }
+
+  return [lang, title];
+}
+
+/**
+Allows for the following syntax:
+````md
+```js[my code]
+let someCode = 100;
+```
+````
+To add a `data-title="my code"` attribute to the code html code block
+ */
+const remarkCodeDataTitles: Plugin<[], Root> = () => 
+{
+  const transform: Transformer<Root> = (tree) => {
+    visit(tree, 'code', (node) => {
+      const data = node.data || (node.data = {})
+      const properties = data.hProperties || (data.hProperties = {})
+
+      const [lang, title] = getLangAndTitle(node);
+
+      if (!lang || !title) return;
+
+      properties['data-title'] = title;
+      node.lang = lang;
+    })
+  };
+  
+  return transform;
+};
+
+export default remarkCodeDataTitles;
