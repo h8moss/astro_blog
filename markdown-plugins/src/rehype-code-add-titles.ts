00c9b9fc0235fed365ca9e7a349b15d657359585
h8m0ss <danarmenta32@gmail.com>
2023-11-08 13:22:24 -0600

    FEAT: Basics of second plugin

---
+++ markdown-plugins/src/rehype-code-add-titles.ts
@@ -0,0 +1,20 @@
+import { visit } from "unist-util-visit";
+import { Plugin, Transformer } from "unified";
+import type { Root } from "remark-directive/lib";
+
+
+
+const rehypeCodeAddTitles: Plugin<[], Root> = () => {
+  const transform: Transformer<Root> = (tree) => {
+    visit(tree, 'code', (node) => {
+      const data = node.data || (node.data = {})
+      const properties = data.hProperties || (data.hProperties = {})
+
+      // TODO Implement this
+    })
+  };
+
+  return transform;
+};
+
+export default rehypeCodeAddTitles;
