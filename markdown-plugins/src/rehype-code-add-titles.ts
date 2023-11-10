6e9605b406031e9ef7224b8e5fc245f4a3aaa4cf
h8m0ss <danarmenta32@gmail.com>
2023-11-09 20:54:49 -0600

    FEAT: Implemented rehype-code-add-titles

--- markdown-plugins/src/rehype-code-add-titles.ts
+++ markdown-plugins/src/rehype-code-add-titles.ts
@@ -1,14 +1,46 @@
-import { visit } from "unist-util-visit";
+import { visit, SKIP } from "unist-util-visit";
 import { Plugin, Transformer } from "unified";
-import type { Root } from "remark-directive/lib";
+import type { Root, Element } from "hast";
+import { h } from "hastscript";
 
+/**
+Turns all 
+```html
+<pre>
+<code data-title="title">code</code>
+</pre>
+```
+blocks into
+```html
+<pre>
+<div>
+	<p class="title">title</p>
+	<code>code</code>
+</div>
+</pre>
+```
+*/
 const rehypeCodeAddTitles: Plugin<[], Root> = () => {
 	const transform: Transformer<Root> = (tree) => {
-		visit(tree, "code", (node) => {
-			const data = node.data || (node.data = {});
-			const properties = data.hProperties || (data.hProperties = {});
+		visit(tree, { tagName: "pre" }, (node) => {
+			const child = node.children.find((child) => child.type === "element") as
+				| Element
+				| undefined;
 
-			// TODO Implement this
+			if (!child) return;
+
+			const title =
+				child.properties["data-title"] || child.properties.dataTitle; // I dont know why, but in astro it gets translated to dataTitle
+
+			if (!title || typeof title !== "string") return;
+
+			console.log({ node, title, child, count: 2 });
+
+			node.tagName = "div";
+			node.properties["class"] = "code-container";
+			node.children = [h("p", title), h("pre", child)];
+
+			return SKIP; // To avoid going into an infinite loop
 		});
 	};
 
