db10767004d159798e8a833e4e6380673f3be310
h8m0ss <danarmenta32@gmail.com>
2023-11-17 22:25:50 -0600

    FEAT: Added usage of markdown-plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,30 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
+import { remarkCodeDataTitles, rehypeCodeAddTitles, remarkAddMissingCodeTitles } from 'markdown-plugins'
+import rehypeShiki from 'rehype-shiki';
+
 const defaultMarkdownLayout = () => {
   return function (_, file) {
-    file.data.astro.frontmatter.layout = file.data.astro.frontmatter.layout || "../../../layouts/MarkdownLayout.astro";
+    file.data.astro.frontmatter.layout =
+      file.data.astro.frontmatter.layout
+      || "../../../layouts/MarkdownLayout.astro";
   };
 };
-
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react(), svelte()],
   markdown: {
-    remarkPlugins: [defaultMarkdownLayout],
-    shikiConfig: {
-      theme: 'monokai'
-    }
+    remarkPlugins: [defaultMarkdownLayout, remarkCodeDataTitles, remarkAddMissingCodeTitles],
+    rehypePlugins: [
+      rehypeCodeAddTitles,
+      [rehypeShiki, {
+        useBackground: false,
+        theme: 'monokai'
+      }] // Add custom syntax highlighting
+    ],
+    syntaxHighlight: false, // Prism's syntax highlight kills rehypeCodeAddTitles
   }
 });
