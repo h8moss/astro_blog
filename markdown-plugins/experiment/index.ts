6ed0621e640acbf382491a895bcc5388eadacc38
h8m0ss <danarmenta32@gmail.com>
2023-11-17 22:25:00 -0600

    CHORE: Updated markdown-plugins experiment

--- markdown-plugins/experiment/index.ts
+++ markdown-plugins/experiment/index.ts
@@ -1,16 +1,15 @@
-import rehypeSanitize from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
-import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import remarkCodeDataTitles from "../src/remark-code-data-titles.js";
 import { remark } from "remark";
 import logger from "./logger.js";
 import rehypeCodeAddTitles from "../src/rehype-code-add-titles.js";
+import remarkAddMissingCodeTitles from "../src/remark-add-missing-code-titles.js";
 
 const text = `
 # hello world
 
-\`\`\`js[my nice code]
+\`\`\`js
 let x = 100;
 \`\`\`
 
@@ -20,6 +19,7 @@ let x = 100;
 
 const file = await remark()
 	.use(remarkCodeDataTitles)
+	.use(remarkAddMissingCodeTitles)
 	.use(remarkRehype)
 	.use([logger])
 	.use(rehypeCodeAddTitles)
