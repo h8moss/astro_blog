ed57f89caaadac173c41e2b8f71b12156a767805
h8m0ss <danarmenta32@gmail.com>
2023-11-19 13:00:51 -0600

    FIX: Added missing export from plugins index

--- markdown-plugins/src/index.ts
+++ markdown-plugins/src/index.ts
@@ -2,9 +2,11 @@ import remarkCodeDataTitles from "./remark-code-data-titles.js";
 import remarkAddMissingCodeTitles from "./remark-add-missing-code-titles.js";
 
 import rehypeCodeAddTitles from "./rehype-code-add-titles.js";
+import rehypeHeadingIds from "./rehype-heading-ids.js";
 
 export {
 	remarkCodeDataTitles,
 	remarkAddMissingCodeTitles,
-	rehypeCodeAddTitles
+	rehypeCodeAddTitles,
+	rehypeHeadingIds
 };
